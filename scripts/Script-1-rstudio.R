295858f4bb7eba8a8023270bcdcbaa0637d5247e
Mark Andrews <mjandrews.org@gmail.com>
2026-04-28 16:20:31 +0100

    Update Script 1

--- scripts/Script-1-rstudio.R
+++ scripts/Script-1-rstudio.R
@@ -28,6 +28,24 @@ server <- function(input, output){
 
 shinyApp(ui, server)
 
-# this one? ---------------------------------------------------------------
 
-# this ..... --------------------------------------------------------------
+# Histogram with 4 sliders ------------------------------------------------
+
+library(tidyverse)
+
+ui <- fluidPage(
+  sliderInput("bins", "Number of histogram bins", min = 10, max = 100, value = 30),
+  plotOutput("histogram")
+)
+
+server <- function(input, output){
+  output$histogram <- renderPlot(
+    {
+      data_df <- tibble(x = rnorm(n = 100, mean = 100, sd = 15))    
+      ggplot(data_df, aes(x = x)) + 
+        geom_histogram(bins = input$bins)
+    }
+  )  
+}
+
+shinyApp(ui, server)
